b7cd7ef58f4536b8f00b25d9493efb657899c057
chenjilan123 <30375990+chenjilan123@users.noreply.github.com>
2019-03-04 22:19:10 +0800

    1

--- CSharp/CSharp/Program.cs
+++ CSharp/CSharp/Program.cs
@@ -6,21 +6,7 @@ namespace CSharp
     {
         static void Main(string[] args)
         {
-            var jetArr = new JetArray()
-            {
-                new Jet() { Code = "NF5" },
-                new Jet() { Code = "NH583"},
-                new Jet() { Code = "FL440"},
-                new Jet() { Code = "FL441"},
-                new JetArray()
-                {
-                    new Jet() { Code = "FY4" },
-                },
-            };
-            foreach (var jet in jetArr)
-            {
-                Console.WriteLine(jet.Code);
-            }
+            Console.WriteLine("Hello World.");
         }
     }
 }
