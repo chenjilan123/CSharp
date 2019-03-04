53503572e21fae8c0240aae76242f5adc3276e8f
chenjilan123 <30375990+chenjilan123@users.noreply.github.com>
2019-03-04 22:15:48 +0800

    1

---
+++ CSharp/CSharp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var jetArr = new JetArray()
+            {
+                new Jet() { Code = "NF5" },
+                new Jet() { Code = "NH583"},
+                new Jet() { Code = "FL440"},
+                new Jet() { Code = "FL441"},
+                new JetArray()
+                {
+                    new Jet() { Code = "FY4" },
+                },
+            };
+            foreach (var jet in jetArr)
+            {
+                Console.WriteLine(jet.Code);
+            }
+        }
+    }
+}
