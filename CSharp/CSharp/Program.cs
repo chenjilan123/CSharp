c2f41f3966bb21e5f7611352da12bcd1ce0cd78d
11 <11@DESKTOP-M5DA0GK>
2019-03-06 15:48:12 +0800

    1

--- CSharp/CSharp/Program.cs
+++ CSharp/CSharp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading;
+using System.Timers;
 
 namespace CSharp
 {
@@ -6,7 +8,27 @@ namespace CSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World.");
+            var timer = new System.Timers.Timer(3000);
+            timer.Elapsed += Timer_Elapsed;
+            timer.Start();
+
+            Console.WriteLine("Press \"Enter\" to quit");
+            Console.ReadLine();
+
+            timer.Stop();
+        }
+
+        private static void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            var id = Guid.NewGuid().ToString();
+            var round = 1;
+            while(true)
+            {
+                Console.WriteLine($"{id}: {round++}");
+                Thread.Sleep(5000);
+                ThreadPool.GetAvailableThreads(out int threads, out int completionPortThreads);
+                Console.WriteLine($"Threads: {threads}, CompletionPortThreads: {completionPortThreads}");
+            }
         }
     }
 }
