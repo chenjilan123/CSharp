2a02c54e52ecfc615b3a5b83ae2f8a70694ed4d5
11 <11@DESKTOP-M5DA0GK>
2019-03-07 17:53:25 +0800

    1

--- CSharp/CSharp/Program.cs
+++ CSharp/CSharp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NPOI.HSSF.UserModel;
+using System;
 
 namespace CSharp
 {
@@ -6,7 +7,7 @@ namespace CSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World.");
+            HSSFWorkbook workBook;
         }
     }
 }
