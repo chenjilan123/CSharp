88ef5b676bf9f2138d8c3ebff4dda6783fdac48c
chenjilan123 <30375990+chenjilan123@users.noreply.github.com>
2019-03-13 23:08:22 +0800

    1

--- CSharp/CSharp/Tasks/TaskAsyncKeyword.cs
+++ CSharp/CSharp/Tasks/TaskAsyncKeyword.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace CSharp.Tasks
 {
@@ -8,8 +9,19 @@ namespace CSharp.Tasks
     {
         public TaskAsyncKeyword Base()
         {
-
+            Console.WriteLine("Begin.");
+            Async();
+            Console.WriteLine("End all.");
             return this;
+        }
+
+        private async Task Async()
+        {
+            Console.WriteLine("Begin Async.");
+
+            await Task.Delay(5000);
+
+            Console.WriteLine("End async.");
         }
     }
 }
