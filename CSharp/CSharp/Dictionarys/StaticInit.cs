ba182899d94b04c9abf91833421ba6e9e0bd4f15
11 <11@DESKTOP-M5DA0GK>
2019-03-26 17:51:36 +0800

    1

---
+++ CSharp/CSharp/Dictionarys/StaticInit.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp.Dictionarys
+{
+    public class StaticInit
+    {
+        private static Dictionary<string, string> _dic = new Dictionary<string, string>() { { "Heheda", "Hello"} };
+        public static Dictionary<string, string> Dic
+        {
+            get
+            {
+                if (_dic.Count <= 1)
+                {
+                    _dic.Add("Hee", "Hello World!");
+                }
+                return _dic;
+            }
+        }
+
+        public static void Print()
+        {
+            Console.WriteLine(_dic.Count);
+        }
+    }
+}
--- CSharp/CSharp/Program.cs
+++ CSharp/CSharp/Program.cs
@@ -1,6 +1,7 @@
 ﻿using CSharp.Dictionarys;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace CSharp
 {
@@ -17,7 +18,7 @@ namespace CSharp
             //}
             //Console.WriteLine(count);
 
-            RemoveSomeOfDictionary();
+            StaticInits();
         }
 
         #region RemoveSomeOfDictionary
@@ -64,6 +65,45 @@ namespace CSharp
                 Console.WriteLine(item);
             }
             return;
+        }
+        #endregion
+
+        #region Except
+        private static void Except()
+        {
+            var l1 = new[] { 1, 2, 3, 5, 9 };
+            var l2 = new[] { 2, 3, 4, 5, 6, 7};
+
+
+            Console.WriteLine("l1:");
+            foreach (var item in l1)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("l2:");
+            foreach (var item in l2)
+            {
+                Console.WriteLine(item);
+            }
+            var except = l1.Except(l2);
+            Console.WriteLine("l1 except l2");
+            foreach (var item in except)
+            {
+                Console.WriteLine(item);
+            }
+            except = l2.Except(l1);
+            Console.WriteLine("l2 except l1");
+            foreach (var item in except)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        #endregion
+
+        #region StaticInits
+        private static void StaticInits()
+        {
+            StaticInit.Print();
         }
         #endregion
     }
