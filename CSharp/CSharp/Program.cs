d4d1d50c814df5e7f38dbab806590499aff339eb
11 <11@DESKTOP-M5DA0GK>
2019-03-28 10:24:31 +0800

    1

--- CSharp/CSharp/Program.cs
+++ CSharp/CSharp/Program.cs
@@ -10,5 +10,21 @@ namespace CSharp
             new EntityToXml()
                 .Serialize();
         }
+
+        private static void InterfaceTest()
+        {
+            Intf i = new Imple();
+            Console.WriteLine(i.ToString());
+        }
+    }
+
+    public interface Intf
+    {
+        int MyProperty { get; set; }
+    }
+
+    public class Imple : Intf
+    {
+        public int MyProperty { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
     }
 }
