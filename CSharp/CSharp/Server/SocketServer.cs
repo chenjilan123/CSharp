b140a89779b321d27530f91a58263a473a9ba4cb
chenjilan123 <30375990+chenjilan123@users.noreply.github.com>
2019-04-07 22:06:07 +0800

    1

---
+++ CSharp/CSharp/Server/SocketServer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+
+namespace CSharp.Server
+{
+    public class SocketServer
+    {
+        public void RunDemo()
+        {
+            var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socket.Connect("127.0.0.1", 8088);
+
+        }
+    }
+}
