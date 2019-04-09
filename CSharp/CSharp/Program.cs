a11e047272df25ebf169456f79ccd93e07ee4496
chenjilan123 <30375990+chenjilan123@users.noreply.github.com>
2019-04-09 19:51:33 +0800

    1

--- CSharp/CSharp/Program.cs
+++ CSharp/CSharp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CSharp.Server;
+using System;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
@@ -13,59 +14,12 @@ namespace CSharp
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-            const string localHost = "127.0.0.1";
-            const string baidu = "www.baidu.com";
-            const string sina = "www.sina.com.cn";
-            const string sinasports = "sports.sina.com.cn";
-            //const string baidu1 = "14.215.177.38";
+            Socket();
+        }
 
-            var server = baidu;
-            Socket socket = null;
-
-            var hostEntry = Dns.GetHostEntry(server);
-            if (hostEntry.AddressList.Length <= 0)
-            {
-                Console.WriteLine("No address");
-            }
-            foreach (var address in hostEntry.AddressList)
-            {
-                //Console.WriteLine(address);
-                var ipe = new IPEndPoint(address, 80);
-                Socket tempSocket = new Socket(ipe.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-                tempSocket.Connect(ipe);
-                if (tempSocket.Connected)
-                {
-                    Console.WriteLine($"Socket connected, Address Type: {ipe.AddressFamily}");
-                    socket = tempSocket;
-                    break;
-                }
-            }
-            if (socket == null)
-            {
-                Console.WriteLine("No socket connected.");
-                return;
-            }
-            var request = $"GET /HTTP/1.1\r\n" +
-                $"Host: {server}\r\n" +
-                $"Connection: Close\r\n\r\n";
-            var bytesSent = Encoding.ASCII.GetBytes(request);
-            var bytesReceived = new byte[256];
-            string page = "";
-            using (socket)
-            {
-                socket.Send(bytesSent, bytesSent.Length, 0);
-                int bytes = 0;
-                page = $"Default HTML page on {server}:\r\n";
-                do
-                {
-                    bytes = socket.Receive(bytesReceived, bytesReceived.Length, 0);
-                    page += Encoding.ASCII.GetString(bytesReceived, 0, bytes);
-                }
-                while (bytes > 0);
-            }
-
-            Console.WriteLine(page);
-
+        static private void Socket()
+        {
+            new SocketServer().Run();
         }
     }
 }
--- CSharp/CSharp/Server/SocketServer.cs
+++ CSharp/CSharp/Server/SocketServer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
@@ -7,12 +8,72 @@ namespace CSharp.Server
 {
     public class SocketServer
     {
-        public void RunDemo()
+        public void Run()
         {
+            this.RunPage();
+            return;
+
             var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             socket.Connect("127.0.0.1", 8088);
 
             
+        }
+
+        private void RunPage()
+        {
+            const string localHost = "127.0.0.1";
+            const string baidu = "www.baidu.com";
+            const string sina = "www.sina.com.cn";
+            const string sinasports = "sports.sina.com.cn";
+            //const string baidu1 = "14.215.177.38";
+
+            var server = sina;
+            Socket socket = null;
+
+            var hostEntry = Dns.GetHostEntry(server);
+            if (hostEntry.AddressList.Length <= 0)
+            {
+                Console.WriteLine("No address");
+            }
+            foreach (var address in hostEntry.AddressList)
+            {
+                //Console.WriteLine(address);
+                var ipe = new IPEndPoint(address, 80);
+                Socket tempSocket = new Socket(ipe.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                tempSocket.Connect(ipe);
+                if (tempSocket.Connected)
+                {
+                    Console.WriteLine($"Socket connected, Address Type: {ipe.AddressFamily}");
+                    socket = tempSocket;
+                    break;
+                }
+            }
+            if (socket == null)
+            {
+                Console.WriteLine("No socket connected.");
+                return;
+            }
+            var request = $"GET /HTTP/1.1\r\n" +
+                $"Host: {server}\r\n" +
+                $"Connection: Close\r\n\r\n";
+            var bytesSent = Encoding.ASCII.GetBytes(request);
+            var bytesReceived = new byte[256];
+            string page = "";
+            using (socket)
+            {
+                socket.Send(bytesSent, bytesSent.Length, 0);
+                int bytes = 0;
+                page = $"Default HTML page on {server}:\r\n";
+                do
+                {
+                    bytes = socket.Receive(bytesReceived, bytesReceived.Length, 0);
+                    page += Encoding.ASCII.GetString(bytesReceived, 0, bytes);
+                }
+                while (bytes > 0);
+            }
+
+            Console.WriteLine(page);
+
         }
     }
 }
