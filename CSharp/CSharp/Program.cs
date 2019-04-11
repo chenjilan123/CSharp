7a8193de9dca17a1fe2b7aee48fd8c1a1bdacb80
11 <11@DESKTOP-M5DA0GK>
2019-04-11 17:41:54 +0800

    1

--- CSharp/CSharp/Program.cs
+++ CSharp/CSharp/Program.cs
@@ -1,5 +1,7 @@
-﻿using CSharp.xml;
+﻿using CSharp.Entity;
+using CSharp.xml;
 using System;
+using System.Runtime.InteropServices;
 
 namespace CSharp
 {
@@ -9,10 +11,35 @@ namespace CSharp
         static void Main(string[] args)
         {
             //Console.WriteLine(IsTrue);
+            SizeMeasure();
 
-            new EntityToXml()
-                .Serialize();
+            //new EntityToXml()
+            //    .Serialize();
         }
+
+        #region SizeMeasure
+        private static void SizeMeasure()
+        {
+            uint u = 0;
+            var b1 = Marshal.SizeOf(u);
+            Console.WriteLine($"Size of uint: {b1}");
+            int i = 0;
+            var b2 = Marshal.SizeOf(i);
+            Console.WriteLine($" Size of int: {b2}");
+            long l = 0;
+            var b3 = Marshal.SizeOf(l);
+            Console.WriteLine($"Size of long: {b3}");
+
+            //Error
+            //var x = sizeof(Pilot);
+            //Error
+            //Console.WriteLine($"Size of Polot: {Marshal.SizeOf(typeof(Pilot))}");
+            //Error： cant be marshaled
+            //var pilot = new Pilot();
+            //var b4 = Marshal.SizeOf(pilot);
+            //Console.WriteLine($"Size of pilot: {b4}");
+        }
+        #endregion
 
         private static void InterfaceTest()
         {
