edf30542c7ecad7fe08148f9c0b57720f45a59ae
chenjilan123 <30375990+chenjilan123@users.noreply.github.com>
2019-04-14 21:41:17 +0800

    1

---
+++ CSharp/CSharp/Ping/IcmPacket.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp.Ping
+{
+    public class IcmPacket
+    {
+        private byte type;
+        private byte subCode;
+        private ushort checkSum;
+        private ushort identifier;
+        private ushort sequenceNumber;
+        private byte[] data;
+
+        public ushort CheckSum
+        {
+            get
+            {
+                return checkSum;
+            }
+            set
+            {
+                checkSum = value;
+            }
+        }
+
+        public IcmPacket(byte type, byte subCode, ushort checkSum, ushort identifier, ushort sequenceNumber, int dataSize)
+        {
+            this.type = type;
+            this.subCode = subCode;
+            this.checkSum = checkSum;
+            this.identifier = identifier;
+            this.sequenceNumber = sequenceNumber;
+            this.data = new byte[dataSize];
+            for (int i = 0; i < dataSize; i++)
+            {
+                data[i] = (byte)'k';
+            }
+        }
+
+        public int CountByte(byte[] buffer)
+        {
+            var type = new [] { this.type };
+            var code = new[] { this.subCode };
+            var cksum = BitConverter.GetBytes(this.checkSum);
+            var id = BitConverter.GetBytes(this.identifier);
+            var seq = BitConverter.GetBytes(this.sequenceNumber);
+            int i = 0;
+            Array.Copy(type, 0, buffer, i, type.Length);
+            i += type.Length;
+            Array.Copy(code, 0, buffer, i, code.Length);
+            i += code.Length;
+            Array.Copy(cksum, 0, buffer, i, cksum.Length);
+            i += cksum.Length;
+            Array.Copy(id, 0, buffer, i, id.Length);
+            i += id.Length;
+            Array.Copy(seq, 0, buffer, i, seq.Length);
+            i += seq.Length;
+            Array.Copy(data, 0, buffer, i, data.Length);
+            i += data.Length;
+            return i;
+        }
+
+        public static ushort SumOfCheck(ushort[] buffer)
+        {
+            int cksum = 0;
+            for (int i = 0; i < buffer.Length; i++)
+                cksum += (int)buffer[i];
+            cksum = (cksum >> 16) + (cksum & 0xffff);
+            return (ushort)(~cksum);
+        }
+    }
+}
--- CSharp/CSharp/Ping/PingDemo.cs
+++ CSharp/CSharp/Ping/PingDemo.cs
@@ -3,21 +3,99 @@ using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Linq;
 
 namespace CSharp.Ping
 {
     public class PingDemo
     {
+        private const int SendTime = 5000;
         public void Start()
         {
             var hostInfo = GetHostInfo();
             var socket = new Socket(AddressFamily.InterNetwork, SocketType.Raw, ProtocolType.Icmp);
             socket.ReceiveTimeout = 1000;
 
-            EndPoint ep = new IPEndPoint(hostInfo.AddressList[0], 0);
+            var ipAddress = hostInfo.AddressList.Where(ipAddr => ipAddr.AddressFamily == AddressFamily.InterNetwork).FirstOrDefault();
+            if (ipAddress == null)
+            {
+                Console.WriteLine("There is not any ipv4 address in the host!");
+                return;
+            }
 
+            EndPoint hostEp = new IPEndPoint(ipAddress, 0);
+            var clientInfo = hostInfo;
+            EndPoint clientEp = new IPEndPoint(ipAddress, 0);
 
-            Console.WriteLine("End");
+            var dataSize = 4;
+            var packetSize = dataSize + 8;
+            const int Icmp_echo = 8;
+            var packet = new IcmPacket(Icmp_echo, 0, 0, 45, 0, dataSize);
+            var buffer = new byte[packetSize];
+            int index = packet.CountByte(buffer);
+            if (index != packetSize)
+            {
+                Console.WriteLine("Reporter data error!");
+                return;
+            }
+            var cksumBufferLength = (int)Math.Ceiling(((double)index) / 2);
+            var cksumBuffer = new ushort[cksumBufferLength];
+            var icmpHeaderBufferIndex = 0;
+            for (int i = 0; i < cksumBufferLength; i++)
+            {
+                cksumBuffer[i] = BitConverter.ToUInt16(buffer, icmpHeaderBufferIndex);
+                icmpHeaderBufferIndex += 2;
+            }
+            packet.CheckSum = IcmPacket.SumOfCheck(cksumBuffer);
+            var sendData = new byte[packetSize];
+            index = packet.CountByte(sendData);
+            if (index != packetSize)
+            {
+                Console.WriteLine("Reporter data error!");
+                return;
+            }
+            for (int i = 0; i < SendTime; i++)
+            {
+                var nBytes = 0;
+                var startTime = Environment.TickCount;
+                try
+                {
+                    nBytes = socket.SendTo(sendData, packetSize, SocketFlags.None, clientEp);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Unable to send reporter!");
+                    return;
+                }
+                var receiveData = new byte[256];
+                nBytes = 0;
+                var timeConsume = 0;
+                while(true)
+                {
+                    try
+                    {
+                        nBytes = socket.ReceiveFrom(receiveData, 256, SocketFlags.None, ref clientEp);
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("Timeout without response!");
+                        return;
+                    }
+                    if (nBytes > 0)
+                    {
+                        timeConsume = Environment.TickCount - startTime;
+                        if (timeConsume < 1)
+                        {
+                            Console.WriteLine($"Reply from: {ipAddress.ToString()} Send: {packetSize + 20}time<1ms; bytes received {nBytes}");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Reply from: {ipAddress.ToString()} Send: {packetSize + 20} in {timeConsume}ms; bytes received: {nBytes}");
+                        }
+                        break;
+                    }
+                }
+            }
         }
 
         private IPHostEntry GetHostInfo()
@@ -26,8 +104,8 @@ namespace CSharp.Ping
             bool isHostRight = false;
             while (!isHostRight)
             {
-                Console.WriteLine("Please input the host address: ");
-                var hostName = Console.ReadLine();
+                //Console.WriteLine("Please input the host address: ");
+                var hostName = "127.0.0.1"; //Console.ReadLine();
 
                 try
                 {
