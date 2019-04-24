314a1547d0a4f02c3284ff859ff038794fa096b5
11 <11@DESKTOP-M5DA0GK>
2019-04-24 08:50:52 +0800

    1

--- CSharp/CSharp.Framework/Program.cs
+++ CSharp/CSharp.Framework/Program.cs
@@ -1,16 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
-namespace CSharp
+namespace CSharp.Framework
 {
     class Program
     {
         private const int PadLength = 30;
+
         static void Main(string[] args)
         {
             System.Uri uri = new Uri(typeof(string).Assembly.CodeBase);
             string runtimePath = System.IO.Path.GetDirectoryName(uri.LocalPath);
             string installUtilPath = System.IO.Path.Combine(runtimePath, "InstallUtil.exe");
-            string a = Environment.CurrentDirectory;
+            string a = System.Windows.Forms.Application.ExecutablePath;
 
             PrintMessage("Runtime Path", runtimePath);
             PrintMessage("Install Util Path", installUtilPath);
