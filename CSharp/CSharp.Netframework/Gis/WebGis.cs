46cbeeb43c2f8f78c625eaa92346ae70122717fc
11 <11@DESKTOP-M5DA0GK>
2019-05-28 15:22:56 +0800

    1

---
+++ CSharp/CSharp.Netframework/Gis/WebGis.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.NetFramework.Gis
+{
+    public class WebGis
+    {
+        public string GetLocation(double dLon, double dLat)
+        {
+            return string.Empty;
+        }
+    }
+}
