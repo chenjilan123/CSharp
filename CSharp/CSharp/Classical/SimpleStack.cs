2b853b4bd2bf1cbbc4b92a3ce9a971053ad4da23
11 <11@DESKTOP-M5DA0GK>
2019-06-06 10:05:22 +0800

    1

---
+++ CSharp/CSharp/Classical/SimpleStack.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp.Classical
+{
+    public class SimpleStack<T>
+    {
+        public void Push(T item)
+        {
+
+        }
+
+        public T Peek()
+        {
+            return default(T);
+        }
+
+        public T Pop()
+        {
+            return default(T);
+        }
+    }
+}
