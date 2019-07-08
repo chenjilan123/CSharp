6383096f4cb8c85773d1c47426d1bdffb3fd7015
11 <11@DESKTOP-M5DA0GK>
2019-07-08 17:06:20 +0800

    1

---
+++ CSharp/CSharp.UnityIoC/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Unity;
+
+namespace CSharp.UnityIoC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IUnityContainer register = new Unity.UnityContainer();
+            //register.RegisterType<ILogger>
+
+            var container = new Unity.UnityContainer();
+
+        }
+    }
+}
