f806c682c637a5c5cc1ef40f446da6f9c77c7730
11 <11@DESKTOP-M5DA0GK>
2019-08-16 17:45:58 +0800

    1

--- CSharp/CSharp.DbTest/Program.cs
+++ CSharp/CSharp.DbTest/Program.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Data.Common;
 using System.Data.SqlClient;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
 using System.Text;
 
@@ -32,11 +35,39 @@ namespace CSharp.DbTest
                 using (DbCommand command = factory.CreateCommand())
                 {
                     command.Connection = connection;
-                    command.CommandText = "UPDATE bas_Driver SET pic = null WHERE DriverName = 'cr7'";
+                    command.CommandText = "UPDATE bas_Driver SET pic = @img WHERE DriverName = 'cr7'";
 
-                    var result = command.ExecuteNonQuery();
-                    Console.WriteLine($"Row Effected: {result}");
+                    var param = factory.CreateParameter();
+                    param.ParameterName = "@img";
+                    var dataLst = GetImgData(Image.FromFile("cr7.jpg"));
+                    param.Value = dataLst;
+
+                    Console.WriteLine(dataLst.Length);
+                    command.Parameters.Add(param);
+
+                    //var result = command.ExecuteNonQuery();
+                    //Console.WriteLine($"Row Effected: {result}");
                 }
+            }
+        }
+
+        static byte[] GetImgData(Image img)
+        {
+            using (var ms = new MemoryStream())
+            {
+                img.Save(ms, ImageFormat.Jpeg);
+                ms.Position = 0;
+                var data = new byte[1024];
+                var dataLst = new List<byte>();
+                var count = ms.Read(data, 0, data.Length);
+                while(count > 0)
+                {
+                    dataLst.AddRange(data.AsEnumerable().Take(count));
+                    //Console.WriteLine($"Read {count}bytes");
+                    //ms.Length;
+                    count = ms.Read(data, dataLst.Count, data.Length);
+                }
+                return dataLst.ToArray();
             }
         }
         #endregion
