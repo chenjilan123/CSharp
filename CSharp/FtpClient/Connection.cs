6ce8f4ace7d4d398ae5aafe2f81d92550321953b
chenjilan123 <30375990+chenjilan123@users.noreply.github.com>
2019-08-27 21:43:56 +0800

    1

---
+++ CSharp/FtpClient/Connection.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace FtpClient
+{
+    public class Connection
+    {
+        private readonly TcpClient _client;
+        private Stream _dataStream;
+        public event Action<byte[], int> OnReceiveData;
+        public Connection()
+        {
+            _client = new TcpClient(AddressFamily.InterNetwork);
+        }
+
+        #region 启动
+        public async Task StartAsync()
+        {
+            await _client.ConnectAsync("127.0.0.1", 21).ConfigureAwait(false);
+            Console.WriteLine("启动成功");
+            try
+            {
+                Task tReceive;
+                _dataStream = _client.GetStream();
+                tReceive = ReceiveAsync(_dataStream);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Unexcepted exceptions happended: {ex.ToString()}");
+            }
+        }
+        #endregion
+
+        #region 接收
+        private async Task ReceiveAsync(Stream stream)
+        {
+            var buffer = new byte[1024];
+            Console.WriteLine("开始接受数据");
+            while (true)
+            {
+                var count = await stream.ReadAsync(buffer, 0, buffer.Length);
+                OnReceiveData(buffer, count);
+            }
+        }
+        #endregion
+
+        #region 发送
+        public async Task SendAsync(byte[] buffer, int offset, int count)
+        {
+            Console.WriteLine($"开始发送数据: {count}bytes");
+            await _dataStream.WriteAsync(buffer, offset, count);
+        }
+        #endregion
+
+        #region 关闭
+        public void Close()
+        {
+            _dataStream.Dispose();
+            _dataStream = null;
+
+            _client.Close();
+
+            Console.WriteLine("已关闭连接");
+        }
+        #endregion
+    }
+}
--- CSharp/FtpServer/Program.cs
+++ CSharp/FtpServer/Program.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using FtpClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 
 namespace FtpServer
 {
@@ -9,7 +11,32 @@ namespace FtpServer
     {
         static void Main(string[] args)
         {
+            StartTcpConnection();
 
+            Console.ReadLine();
+        }
+
+
+        static async void StartTcpConnection()
+        {
+            var conn = new Connection();
+            conn.OnReceiveData += Conn_OnReceiveData;
+
+            //启动连接
+            await conn.StartAsync();
+            //发送数据
+            var data = new byte[] { 1, 2, 3 };
+            await conn.SendAsync(data, 0, data.Length);
+            //等待数据
+            Thread.Sleep(300);
+            //关闭连接
+            conn.Close();
+        }
+
+        private static void Conn_OnReceiveData(byte[] data, int count)
+        {
+            Console.Write($"收: {count.ToString().PadLeft(5, ' ')}bytes, ");
+            Console.WriteLine($"内容: {ASCIIEncoding.ASCII.GetString(data, 0, count)}");
         }
     }
 }
