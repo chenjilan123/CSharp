3da484cae13bb9354e84b14127e19545dfbdef56
11 <11@DESKTOP-M5DA0GK>
2019-08-28 11:21:15 +0800

    1

---
+++ CSharp/CSharp/jt809/Deserializer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp.jt809
+{
+    public class Deserializer
+    {
+        public void Deserize(byte[] input)
+        {
+
+        }
+    }
+}
