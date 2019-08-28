431c42b0c5cbb1c573421d00043584a7aa3d1a76
11 <11@DESKTOP-M5DA0GK>
2019-08-28 11:27:25 +0800

    1

--- CSharp/CSharp/Sms/AliSmsClient.cs
+++ CSharp/CSharp/Sms/AliSmsClient.cs
@@ -1,11 +1,11 @@
-﻿using Aliyun.Acs.Core;
-using Aliyun.Acs.Core.Profile;
-using Aliyun.Acs.Core.Exceptions;
-using Aliyun.Acs.Core.Http;
+﻿//using Aliyun.Acs.Core;
+//using Aliyun.Acs.Core.Profile;
+//using Aliyun.Acs.Core.Exceptions;
+//using Aliyun.Acs.Core.Http;
 using System;
 using System.Collections.Generic;
 using System.Text;
-using static CSharp.Sms.AliSmsConst;
+//using static CSharp.Sms.AliSmsConst;
 
 namespace CSharp.Sms
 {
@@ -13,31 +13,31 @@ namespace CSharp.Sms
     {
         public void Send()
         {
-            IClientProfile profile = DefaultProfile.GetProfile("default", AccessKeyId, AccessKeySecret);
-            DefaultAcsClient client = new DefaultAcsClient(profile);
-            CommonRequest request = new CommonRequest();
-            request.Method = MethodType.POST;
-            request.Domain = "dysmsapi.aliyuncs.com";
-            request.Version = "2017-05-25";
-            request.Action = "SendSms";
-            // request.Protocol = ProtocolType.HTTP;
-            request.AddQueryParameters("PhoneNumbers", PhoneNumbers);
-            request.AddQueryParameters("SignName", SignName);
-            request.AddQueryParameters("TemplateCode", TemplateCode);
-            request.AddQueryParameters("TemplateParam", TemplateParam);
-            try
-            {
-                CommonResponse response = client.GetCommonResponse(request);
-                Console.WriteLine(System.Text.Encoding.Default.GetString(response.HttpResponse.Content));
-            }
-            catch (ServerException e)
-            {
-                Console.WriteLine(e);
-            }
-            catch (ClientException e)
-            {
-                Console.WriteLine(e);
-            }
+            //IClientProfile profile = DefaultProfile.GetProfile("default", AccessKeyId, AccessKeySecret);
+            //DefaultAcsClient client = new DefaultAcsClient(profile);
+            //CommonRequest request = new CommonRequest();
+            //request.Method = MethodType.POST;
+            //request.Domain = "dysmsapi.aliyuncs.com";
+            //request.Version = "2017-05-25";
+            //request.Action = "SendSms";
+            //// request.Protocol = ProtocolType.HTTP;
+            //request.AddQueryParameters("PhoneNumbers", PhoneNumbers);
+            //request.AddQueryParameters("SignName", SignName);
+            //request.AddQueryParameters("TemplateCode", TemplateCode);
+            //request.AddQueryParameters("TemplateParam", TemplateParam);
+            //try
+            //{
+            //    CommonResponse response = client.GetCommonResponse(request);
+            //    Console.WriteLine(System.Text.Encoding.Default.GetString(response.HttpResponse.Content));
+            //}
+            //catch (ServerException e)
+            //{
+            //    Console.WriteLine(e);
+            //}
+            //catch (ClientException e)
+            //{
+            //    Console.WriteLine(e);
+            //}
         }
     }
 }
