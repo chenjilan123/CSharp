e02c30da582d2ed587b53b5d1d3c4a0adef83dcd
11 <11@DESKTOP-M5DA0GK>
2019-08-29 10:23:11 +0800

    1

---
+++ CSharp/CSharp/Sms/AliSmsClient.cs
@@ -0,0 +1,49 @@
+﻿//#define Ali
+
+#if Ali
+using Aliyun.Acs.Core;
+using Aliyun.Acs.Core.Profile;
+using Aliyun.Acs.Core.Exceptions;
+using Aliyun.Acs.Core.Http;
+using static CSharp.Sms.AliSmsConst;
+#endif
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp.Sms
+{
+    public class AliSmsClient
+    {
+        public void Send()
+        {
+#if Ali
+            IClientProfile profile = DefaultProfile.GetProfile("default", AccessKeyId, AccessKeySecret);
+            DefaultAcsClient client = new DefaultAcsClient(profile);
+            CommonRequest request = new CommonRequest();
+            request.Method = MethodType.POST;
+            request.Domain = "dysmsapi.aliyuncs.com";
+            request.Version = "2017-05-25";
+            request.Action = "SendSms";
+            // request.Protocol = ProtocolType.HTTP;
+            request.AddQueryParameters("PhoneNumbers", PhoneNumbers);
+            request.AddQueryParameters("SignName", SignName);
+            request.AddQueryParameters("TemplateCode", TemplateCode);
+            request.AddQueryParameters("TemplateParam", TemplateParam);
+            try
+            {
+                CommonResponse response = client.GetCommonResponse(request);
+                Console.WriteLine(System.Text.Encoding.Default.GetString(response.HttpResponse.Content));
+            }
+            catch (ServerException e)
+            {
+                Console.WriteLine(e);
+            }
+            catch (ClientException e)
+            {
+                Console.WriteLine(e);
+            }
+#endif
+        }
+    }
+}
