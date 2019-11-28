f22a543544b940c36b7de6a3050939d43aac36f2
11 <11@DESKTOP-M5DA0GK>
2019-11-28 14:02:32 +0800

    1

--- CSharp/CSharp/Tcp/NTcpClient/NTcpClient.cs
+++ CSharp/CSharp/Tcp/NTcpClient/NTcpClient.cs
@@ -9,13 +9,23 @@ namespace CSharp.Tcp.NTcpClient
 {
     public class NTcpClient
     {
+        List<TcpClient> Clients = new List<TcpClient>();
         public void Run()
         {
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < 20; i++)
             {
                 Console.WriteLine($"{i}-ThreadId:{Thread.CurrentThread.ManagedThreadId}-准备连接");
                 var t = RunClient(i);
                 Console.WriteLine($"{i}-ThreadId:{Thread.CurrentThread.ManagedThreadId}-连接完毕");
+            }
+            while (true)
+            {
+                var arrClients = Clients.ToArray();
+                foreach (var client in arrClients)
+                {
+                    Send(client);
+                }
+                Thread.Sleep(5000);
             }
             Console.ReadLine();
         }
@@ -25,7 +35,7 @@ namespace CSharp.Tcp.NTcpClient
             var client = new TcpClient();
             await client.ConnectAsync("127.0.0.1", 18000).ConfigureAwait(false);
             Console.WriteLine($"{client.Client.LocalEndPoint}-{i}-ThreadId:{Thread.CurrentThread.ManagedThreadId}-连接成功");
-
+            Clients.Add(client);
             //有一定概率回到(主线程)上下文线程
             //var stream = client.GetStream();
             //while (client.Connected)
@@ -48,30 +58,50 @@ namespace CSharp.Tcp.NTcpClient
             //Console.WriteLine($"{client.Client.LocalEndPoint}-{i}-ThreadId:{Thread.CurrentThread.ManagedThreadId}-断开连接");
             //return;
 
-            //这样不宜多个任务
-            var t = new Task(() =>
+            //这样不宜多个任务，线程池线程不宜长时间运行。
+            //var t = new Task(() =>
+            //{
+            //    var stream = client.GetStream();
+            //    while (client.Connected)
+            //    {
+            //        try
+            //        {
+            //            var data = new byte[]
+            //            {
+            //                0x5B, 0x11, 0x22, 0x33, 0x44, 0x5D
+            //            };
+            //            Console.WriteLine($"{client.Client.LocalEndPoint}-{i}-ThreadId:{Thread.CurrentThread.ManagedThreadId}-开始发送");
+            //            stream.Write(data, 0, data.Length);
+            //            Thread.Sleep(5000);
+            //        }
+            //        catch (Exception ex)
+            //        {
+            //            Console.WriteLine(ex.Message);
+            //        }
+            //    }
+            //    Console.WriteLine($"{client.Client.LocalEndPoint}-{i}-ThreadId:{Thread.CurrentThread.ManagedThreadId}-断开连接");
+            //});
+            //t.Start();
+        }
+
+        private void Send(TcpClient client)
+        {
+            var stream = client.GetStream();
+            try
             {
-                var stream = client.GetStream();
-                while (client.Connected)
+                var data = new byte[]
                 {
-                    try
-                    {
-                        var data = new byte[]
-                        {
-                        0x5B, 0x11, 0x22, 0x33, 0x44, 0x5D
-                        };
-                        Console.WriteLine($"{client.Client.LocalEndPoint}-{i}-ThreadId:{Thread.CurrentThread.ManagedThreadId}-开始发送");
-                        stream.Write(data, 0, data.Length);
-                        Thread.Sleep(5000);
-                    }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine(ex.Message);
-                    }
-                }
-                Console.WriteLine($"{client.Client.LocalEndPoint}-{i}-ThreadId:{Thread.CurrentThread.ManagedThreadId}-断开连接");
-            });
-            t.Start();
+                            0x5B, 0x11, 0x22, 0x33, 0x44, 0x5D
+                };
+                Console.WriteLine($"{client.Client.LocalEndPoint}-ThreadId:{Thread.CurrentThread.ManagedThreadId}-开始发送");
+                stream.Write(data, 0, data.Length);
+                Console.WriteLine($"{client.Client.LocalEndPoint}-ThreadId:{Thread.CurrentThread.ManagedThreadId}-发送完毕");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            //Console.WriteLine($"{client.Client.LocalEndPoint}-ThreadId:{Thread.CurrentThread.ManagedThreadId}-断开连接");
         }
     }
 }
