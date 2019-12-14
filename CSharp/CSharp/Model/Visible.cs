841d927f10ce91afcce02ab283bfbe7d27cfebb7
chenjilan123 <30375990+chenjilan123@users.noreply.github.com>
2019-12-14 23:40:00 +0800

    0

--- CSharp/CSharp/Model/Visible.cs
+++ CSharp/CSharp/Model/Visible.cs
@@ -56,12 +56,65 @@ namespace CSharp.Model
         //public sealed void M3() { }
     }
 
+    public sealed class Viud3
+    {
+        //public virtual void Run() { };
+    }
+
     public static class Vid1
     { 
         //public virtual static void Run() { }
     }
-    //public static class Vid2 : Vid1
-    //{
+    //public static class Vid2 : Vid1 { }
 
-    //}
+    public struct Vidx
+    {
+        //public virtual void Run() { }
+    }
+
+    public class V1 
+    {
+        public virtual void M1() { }
+        //public sealed void M2() { }
+    }
+    public class V2 : V1 
+    {
+        public sealed override void M1()
+        //public override void M1()
+        {
+            base.M1();
+        }
+
+    }
+
+    public class V3 : V2
+    {
+        public override void M1()
+        {
+            base.M1();
+            //无法直接访问V2.M1
+            //base.base.M1();
+        }
+    }
+
+    public class Vx1
+    {
+        private int i1;
+        protected int i2;
+        public int i3;
+        protected void Run() { }
+    }
+
+    public static class Vx1Extention
+    {
+        public static void Running(this Vx1 v)
+        {
+            //不可访问
+            //v.Run();
+            //v.i1;
+            //v.i2;
+            //可访问
+            //v.i3;
+        }
+    }
 }
