ab5b384fd2a68860f4baf362ea295af56e66aeab
chenjilan123 <30375990+chenjilan123@users.noreply.github.com>
2019-12-23 01:35:49 +0800

    0

--- CSharp/CSharp/Model/ParameterProperty.cs
+++ CSharp/CSharp/Model/ParameterProperty.cs
@@ -1,17 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace CSharp.Model
 {
-    class ParameterProperty
+    sealed class ParameterProperty
     {
-        public bool this[int i, int j]
+        //自定义属性名称
+        //[IndexerName("Parameter")]  //编译器特性
+        //protected bool this[int i, int j]
+        bool this[int i, int j]
         {
             get
             {
                 return true;
             }
+        }
+
+        //[IndexerName("Parameter")]
+        //public bool this[int i]
+        //{
+        //    get
+        //    {
+        //        return true;
+        //    }
+        //}
+
+        //protected bool this[int i]
+        //{
+        //    public get { return true; }
+        //    set { }
+        //}
+
+
+        //索引器的属性名为Item, 不能再定义Item属性
+        //public bool Item { get; set; }
+
+        //与索引器方法签名重复了
+        //public bool get_Item(int i, int j)
+        //{
+        //    return true;
+        //}
+
+        public bool GetItem(int i, int j)
+        {
+            //访问语法
+            return this[i, j];
         }
     }
 }
