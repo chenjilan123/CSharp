776b8ca2e21dab449a6fbed2bebcbc935aa7f596
11 <11@DESKTOP-M5DA0GK>
2019-12-24 14:58:28 +0800

    0

---
+++ CSharp/CSharp.Framework/Helper/WebSvc.cs
@@ -0,0 +1,17 @@
+﻿using CSharp.Framework.WebReference;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharp.Framework.Helper
+{
+    public class WebSvc
+    {
+        public void Run()
+        {
+            BusinessOrderHttpService service = new BusinessOrderHttpService();
+            service.transServices("s");
+        }
+    }
+}
