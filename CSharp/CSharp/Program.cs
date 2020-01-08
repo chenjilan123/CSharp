e8b8372cc770867965653ef00de5cbcac197b9fd
chenjilan123 <30375990+chenjilan123@users.noreply.github.com>
2020-01-08 21:58:58 +0800

    0

--- CSharp/CSharp/Program.cs
+++ CSharp/CSharp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 
 namespace CSharp
@@ -7,7 +8,21 @@ namespace CSharp
     {
         static void Main(string[] args)
         {
-            while(true)
+            GetFramework();
+        }
+
+        static void GetFramework()
+        {
+            Console.WriteLine($"OS: {Environment.OSVersion}");
+            Console.WriteLine($"x64: {Environment.Is64BitOperatingSystem}");
+            Console.WriteLine($"Version: {Environment.Version}");
+            Console.WriteLine($"Runtime: {RuntimeInformation.FrameworkDescription}");
+            Console.WriteLine($"OS: {RuntimeInformation.OSDescription}");
+        }
+
+        static void PrintTimes()
+        {
+            while (true)
             {
                 Console.WriteLine($"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff")}");
                 Task.Delay(TimeSpan.FromSeconds(1D)).Wait();
